4682aa260dc4f6a91d154c29aef6a534be0b4e91
SolomatovS <SolomatovS@GMail.com>
2022-03-16 10:12:07 +0300

    qqq

--- file_system_library/src/file_system_repository_source.rs
+++ file_system_library/src/file_system_repository_source.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use std::ffi::{OsString, OsStr};
+use std::ffi::OsString;
 use std::path::PathBuf;
 
 use crate::file_system_library::FileSystemLibrary;
@@ -8,7 +8,7 @@ use crate::file_system_library::FileSystemLibrary;
 type Error = Box<dyn std::error::Error>;
 
 pub struct FileSystemRepositorySource {
-   path: Vec<(OsString, bool)>,
+   path: Vec<OsString>,
 }
 
 impl FileSystemRepositorySource {
@@ -18,8 +18,8 @@ impl FileSystemRepositorySource {
       }
    }
 
-   pub fn add_file_path(&mut self, path: OsString, required_for_load: bool) -> &mut Self {
-      self.path.push((path, required_for_load));
+   pub fn add_file_path(&mut self, path: OsString) -> &mut Self {
+      self.path.push(path);
 
       self
    }
@@ -27,12 +27,11 @@ impl FileSystemRepositorySource {
    fn add_files_from_dir_common<F, K>(&mut self, dir: OsString, file_filter: F, file_maker: K) -> Result<&mut Self, Error>
    where
       F: Fn(&PathBuf) -> bool,
-      K: Fn(PathBuf) -> (OsString, bool)
+      K: Fn(PathBuf) -> OsString,
    {
       let dir = match std::fs::read_dir(dir) {
          Ok(dir) => dir,
          Err(e) => {
-            //println!("{:?}", dir);
             println!("{:?}", e);
             return Err(Box::new(e));
       }};
@@ -68,152 +67,27 @@ impl FileSystemRepositorySource {
 
    pub fn add_files_from_dir<K>(&mut self, dir: OsString, extension: OsString, file_maker: K) -> Result<&mut Self, Error>
    where
-      K: Fn(PathBuf) -> (OsString, bool)
+      K: Fn(PathBuf) -> OsString,
    {
       self.add_files_from_dir_common(dir, Self::filter_by_extension(extension), file_maker)
    }
 
-   //fn search_dlib_files(dir: &OsString, extension: &OsString) -> Result<Vec<OsString>, Error> {
-   //fn search_dlib_files(dir: &OsString, extension: OsString) -> Result<impl Iterator<Item=OsString>, Error> {
-   pub fn search_dlib_files<'a, F>(dir: &'a OsString, filter: F) -> Result<impl Iterator<Item=OsString>, Error>
+   pub fn build<'a, F>(&mut self, action_for_error: F) -> Vec<FileSystemLibrary>
    where
-      F: FnMut(&std::path::PathBuf) -> bool
+      F: Fn(Error)
    {
-      let dir = match std::fs::read_dir(dir) {
-         Ok(dir) => dir,
-         Err(e) => {
-            println!("{:?}", dir);
-            println!("{:?}", e);
-            return Err(Box::new(e));
-      }};
-
-      let paths = dir.into_iter()
-         .filter_map(|path| {
-            match path {
-               Ok(p) => Some(p.path()),
-               Err(e) => {
-                  println!("{:?}", e);
-                  None
-               }
-            }
-         })
-         .filter(|path| path.is_file())
-/*
-         .filter(move |path| match path.extension() {
-            Some(p) => p == extension,
-            None => false
-         })
-*/
-         .filter(filter)
-         .map(|path| path.into_os_string())
-         //.collect()
-      ;
-      
-      Ok(paths)
-   }
-
-   pub fn build(&mut self) -> Vec<FileSystemLibrary> {
-      self.path.iter()
-         .filter_map(|x| Self::make_lib(x.0.to_os_string()))
-         .collect()
-   }
-
-   fn make_lib(path: OsString) -> Option<FileSystemLibrary> {
-      match FileSystemLibrary::new(path.to_os_string()) {
-         Ok(lib) => Some(lib),
-         Err(e) => {
-            println!("{:?}", &e);
-            None
-         }
-      }
-   }
-
-   /*
-   pub fn build(&mut self, extension: &OsString) -> Vec<FileSystemLibrary> {
-      let lambda = |path: &std::path::PathBuf| match path.extension() {
-         Some(x) => x == extension,
-         None => false
-      };
-
-      self.directory_path.iter()
-         // достаю пути к библиотекам которые расположены в указанной папке dir
-         //.filter_map(|dir| match Self::search_dlib_files(dir, extension) {
-         .filter_map(|dir| match Self::search_dlib_files(dir, lambda) {
-            Ok(x) => Some(x),
-            Err(_) => None,
-         })
-         // объединяю результаты сканирования нескольких папок в один массив
-         .flat_map(|x| x)
-         // загружаю библиотеки по указанным путям
+      let col = self.path.iter()
          .filter_map(|x| {
-            match FileSystemLibrary::new(x) {
-               Ok(lib) => Some(lib),
+            match FileSystemLibrary::new(x.to_os_string()) {
+               Ok(x) => Some(x),
                Err(e) => {
-                  println!("{:?}", &e);
+                  action_for_error(e);
                   None
-               }
+               },
             }
          })
-         .collect()
-   }
-   */
-}
-
-
-/*
-impl FileSystemRepositorySource {
-   fn values(&self) {
-      self.library.iter()
+         .collect();
+      
+      col
    }
 }
-
-
-impl IntoIterator for FileSystemRepositorySource {
-   type IntoIter = impl Iterator<Item=ILibrary>;
-   fn into_iter(self) -> Self::IntoIter;{
-
-   }
-}
-*/
-
-/*
-    impl FileSystemDinamicLibrarySource {
-    pub fn new() -> Self {
-    Self {
-    library: Vec::new()
-    }
-     }
-    
-    pub fn add_from_path(&mut self, path: OsString) -> Result<(), Box<dyn Error>>
-     {
-    let lib: Library;
-    unsafe {
-    lib = Library::new(path.as_os_str())?;
-     }
-    
-    self.library.push(FileSystemLibrary::new(path, lib));
-    
-    Ok(())
-     }
-    }
-    
-    impl IDLibRepositorySource for FileSystemDinamicLibrarySource {
-    }
-    
-    impl Iterator for FileSystemDinamicLibrarySource {
-    type Item = Box<dyn ILibrary>;
-    
-    fn next(&mut self) -> Option<Self::Item> {
-    todo!()
-     }
-    
-     fn get_libs(&self) -> impl Iterator<Item = &dyn ILibrary> {
-    
-     let sdf = self.library.iter()
-     .map(|x| x as &dyn ILibrary)
-     ;
-    
-     sdf
-     }
-    }
-*/
--- host/src/main.rs
+++ host/src/main.rs
@@ -10,51 +10,14 @@ fn main() {
     let mut file_source = FileSystemRepositorySource::new();
 
     let file_source = file_source
-        .add_file_path(OsString::from("/Users/solomatovs/Documents/GitHub/roxbi/say_hello_console/target/debug/libsay_hello_console.dylib"), true)
+        .add_file_path(OsString::from("/Users/solomatovs/Documents/GitHub/roxbi/say_hello_console/target/debug/libsay_hello_console.dylib"))
+        .add_file_path(OsString::from("/Users/solomatovs/Documents/GitHub/roxbi/say_hello_console/target/debug/libsay_hello_console.dylib1"))
+        .add_file_path(OsString::from("/Users/solomatovs/Documents/GitHub/roxbi/say_hello_console/target/debug/libsay_hello_console.dylib"))
     ;
     
-    let libs = file_source.build();
-    
+    let libs = file_source.build(|e| {
+        println!("{}", &e);
+    });
+
     libs.iter().for_each(|x| println!("{}", x));
-    /*
-    let dir = match fs::read_dir(lib_path) {Ok(dir) => dir, Err(e) => {
-        println!("lib path {:?} doesn't exist, error: {:?}", lib_path, e);
-        return;
-    }};
-
-    // поочередно проверяем каждый файл в указанной директории
-    for path in dir {
-        let path = match path {
-            Ok(p) => p,
-            Err(e) => {
-                println!("{:?}", lib_path);
-                println!("{:?}", e);
-                return;
-        }};
-
-        let path = path.path();
-
-        // всё, что не является файлом пропускаем
-        if path.is_file() == false {
-            continue;
-        }
-
-        match path.extension() {
-            Some(extension) =>
-                if extension != lib_extensions {continue},
-            None => continue,
-        }
-
-        println!("try load {:?}", &path);
-        if let Err(e) = loader.add_library(path.into_os_string()) {
-            println!("{:?}", e);
-        }
-    }
-
-    loader.find_symbol::<extern "Rust" fn() -> Box<dyn ISayHelloService>>(OsStr::new("new"))
-        .iter()
-        .map(|x| x())
-        .for_each(|x| x.say_hello())
-    ;
-    */
 }
